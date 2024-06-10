3d8d747e4fa192356d3a3a04d198b6c334e9b4e8
nayiest <naylanadyaw@gmail.com>
2024-06-10 08:25:19 +0700

    h

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,6 @@ Route::get('/', function() {
     return view('welcome');
 });
 
-
-
-
 Route::get('/datasantri',[SantriController::class, 'index'])->name('santri');
 // Route::get('/santri/addsantri',[SantriController::class, 'create'])->name('tambahsantri');
 // Route::post('/santri/store',[SantriController::class, 'store'])->name('storetambah');
