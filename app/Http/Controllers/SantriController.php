4d6e7e2ba7f970048338d8c66dff26097fb93033
nayiest <naylanadyaw@gmail.com>
2024-06-10 20:42:18 +0700

    baru

--- app/Http/Controllers/SantriController.php
+++ app/Http/Controllers/SantriController.php
@@ -100,7 +100,8 @@ class SantriController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $santri = santri::findOrFail($id);
+        $filePath = public_path('storage/images');
+        $santri = Santri::findOrFail($id);
         $santri->nama_santri = $request->nama_santri;
         $santri->jk_santri = $request->jk_santri;
         $santri->angkatan_santri = $request->angkatan_santri;
