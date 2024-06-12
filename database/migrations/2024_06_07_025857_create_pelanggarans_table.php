f8b5c8746cea64c6dbb5a51dabfc30a7292a0094
nayiest <naylanadyaw@gmail.com>
2024-06-12 09:35:12 +0700

    apagitu

--- database/migrations/2024_06_07_025857_create_pelanggarans_table.php
+++ database/migrations/2024_06_07_025857_create_pelanggarans_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('pelanggarans', function (Blueprint $table) {
             $table->id();
+            $table->string('nama_santri');
+            $table->string('nama_pelanggaran');
+            $table->string('kategori_pelanggaran'); 
             $table->timestamps();
         });
     }
