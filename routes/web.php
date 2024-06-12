d2fcbce708e122cb381dd11d7ae4d52797ee49c2
nayiest <naylanadyaw@gmail.com>
2024-06-12 08:35:48 +0700

    web uppdate

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SantriController;
+use App\Http\Controllers\PelanggaranController;
 
 /*
 |--------------------------------------------------------------------------
