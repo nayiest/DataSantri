fae09df3f24dc73bb1d8fe4a3b06df9372626f07
nayiest <naylanadyaw@gmail.com>
2024-06-25 09:21:17 +0700

    hyy

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -75,7 +75,7 @@
             <div class="col-lg-5 col-12">
                 <div id="auth-left">
                     <div class="auth-logo">
-                        <a href="{{ url('/') }}"><img src="{{ asset('template/dist/assets/compiled/svg/logo.svg') }}" alt="Logo"></a>
+                        <a href="{{ url('/') }}"><img src="{{ asset('template/dist/images/logo.png') }}" alt="Logo"></a>
                     </div>
                     <h1 class="auth-title">Log in.</h1>
                     <p class="auth-subtitle mb-5">Log in with your data that you entered during registration.</p>
